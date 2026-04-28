8598a60ea7c9e4213c7df8f038142482b82ca4de
Samo1408 <ab.0ss0mm@gmail.com>
2026-04-28 16:31:51 +0300

    Add files via upload

--- kernel/include/linux/susfs_def.h
+++ kernel/include/linux/susfs_def.h
@@ -31,26 +31,19 @@
 #define CMD_SUSFS_IS_SUS_SU_READY 0x555f0
 #define CMD_SUSFS_SUS_SU 0x60000
 
-#define SUSFS_MAX_LEN_PATHNAME 256 // 256 should address many paths already unless you are doing some strange experimental stuff, then set your own desired length
+#define SUSFS_MAX_LEN_PATHNAME 256
 #define SUSFS_FAKE_CMDLINE_OR_BOOTCONFIG_SIZE 4096
 
-#define TRY_UMOUNT_DEFAULT 0 /* used by susfs_try_umount() */
-#define TRY_UMOUNT_DETACH 1 /* used by susfs_try_umount() */
+#define TRY_UMOUNT_DEFAULT 0
+#define TRY_UMOUNT_DETACH 1
 
 #define SUS_SU_DISABLED 0
-#define SUS_SU_WITH_OVERLAY 1 /* deprecated */
+#define SUS_SU_WITH_OVERLAY 1
 #define SUS_SU_WITH_HOOKS 2
 
-#define DEFAULT_SUS_MNT_ID 100000 /* used by mount->mnt_id */
-#define DEFAULT_SUS_MNT_ID_FOR_KSU_PROC_UNSHARE 1000000 /* used by vfsmount->susfs_mnt_id_backup */
-#define DEFAULT_SUS_MNT_GROUP_ID 1000 /* used by mount->mnt_group_id */
-
-/*
- * inode->i_state => storing flag 'INODE_STATE_'
- * mount->mnt.susfs_mnt_id_backup => storing original mnt_id of normal mounts or custom sus mnt_id of sus mounts
- * task_struct->susfs_last_fake_mnt_id => storing last valid fake mnt_id
- * task_struct->susfs_task_state => storing flag 'TASK_STRUCT_'
- */
+#define DEFAULT_SUS_MNT_ID 100000
+#define DEFAULT_SUS_MNT_ID_FOR_KSU_PROC_UNSHARE 1000000
+#define DEFAULT_SUS_MNT_GROUP_ID 1000
 
 #define INODE_STATE_SUS_PATH BIT(24)
 #define INODE_STATE_SUS_MOUNT BIT(25)
